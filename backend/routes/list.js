6487d97b12394afd179a3253a44d056fea94bac4
Pranav Raju <pranavraju2020@gmail.com>
2024-03-01 12:22:58 +0530

    CRUD done

---
+++ backend/routes/list.js
@@ -0,0 +1,60 @@
+const router = require("express").Router();
+const User = require("../models/user");
+const List = require("../models/list");
+
+// create todo
+router.post("/addTask", async (req, res) => {
+  try {
+    const { title, body, email } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      const list = new List({ title, body, user: existingUser });
+      await list.save().then(() => res.status(200).json({ list }));
+      existingUser.list.push(list); //updating the list in user
+      existingUser.save();
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// update
+router.post("/updateTask/:id", async (req, res) => {
+  try {
+    const { title, body, email } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      const list = await List.findByIdAndUpdate(req.params.id, { title, body });
+      list.save().then(() => {
+        res.status(200).json({ message: "task updated" });
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// delete
+router.post("/deleteTask/:id", async (req, res) => {
+  try {
+    const { email } = req.body;
+    const existingUser = await User.findOneAndUpdate(
+      { email },
+      { $pull: { list: req.params.id } }
+    ); //to remove the id from the list in user
+    if (existingUser) {
+      await List.findByIdAndDelete(req.params.id).then(() => {
+        res.status(200).json({ message: "task deleted" });
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//getTask
+router.get("/getTasks/:id", async (req, res) => {
+  const list = await List.find({ user: req.params.id });
+  res.status(200).json({ list });
+});
+module.exports = router;
